30d717a46f7874e8a4818096f0fc6713bfd5b335
krishna0724 <128584348+krishna0724@users.noreply.github.com>
2023-03-22 15:19:34 +0530

    Add files via upload
    Array

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.Array;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc=new Scanner(System.in);
+        int size= sc.nextInt();
+
+        int marks[]= new int [size];
+        for(int i=0; i<size; i++){
+             marks[i]= sc.nextInt();
+        }
+        for(int i=0; i<size; i++ ){
+            System.out.println(marks[i]);
+        }
+    }
+}
